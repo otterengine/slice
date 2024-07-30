b20f4c4fb844d0760d1b9398b57856543f01f487
Olle Lukowski <ollelukowski@hotmail.com>
2024-07-30 20:26:38 +0200

    Fixed includes for testing

--- test/include/test.h
+++ test/include/test.h
@@ -1,6 +1,10 @@
 #ifndef TEST_H
 #define TEST_H
 
+/* Needed by bake_test */
+#include <stdint.h>
+#include <stdbool.h>
+
 /* This generated file contains includes for project dependencies */
 #include "test/bake_config.h"
 
